f63e0acb69f7d1835ac8c61895e15ac6ae3dd069
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-03-03 20:30:26 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,48 @@
+/*let age = 22;
+switch(true){
+    case age<18:
+        console.log("child");
+        break;
+    case 18<age<40:
+        console.log("Adult");
+        break;
+    case age>40:
+        console.log("Old Age");
+        break;
+    default:
+        console.log("Invalid Age");
+        break;
+}*/
+
+/*var day = "2";
+switch(day){
+    case "0":
+        console.log("Sunday");
+        break;
+    case "1":   
+        console.log("Monday");
+        break;
+    case "2":
+        console.log("Tuesday");
+        break;
+    case "3":
+        console.log("Wednesday");
+        break;
+    case "4":
+        console.log("Thursday");
+        break;
+    case "5":
+        console.log("Friday");
+        break;
+    case "6":  
+        console.log("Saturday");
+        break;
+}*/
+
+/*for(var i = 1 ; i <= 10 ; i++){
+    console.log(i);
+}*/
+
+/*for(var i = 1 ; i <=10 ; i++){
+    console.log("19 x "+i+" ="+" "+19*i);
+}*/
