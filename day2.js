9ce35c42f631d9af8ba6e12574224bcfd3da817b
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-03-07 21:08:45 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,57 @@
+/*let a = 10
+switch(true){
+    case a >0:
+        return "positive"
+        break;
+    case a <0:
+        return "negative"
+        break;
+    default:
+        return "its 0"
+        break;
+}*/
+
+/*------------------------*/
+
+/*let i=0;
+for (; i<9 ; i++){
+    console.log(i);
+}*
+
+/*------------------------*/
+
+/*let i = 0 ;
+do{
+    console.log("value of i "+i);
+    i++;
+}
+while(i<10)*/
+
+/*------------------------*/
+
+/*function addNum(x,y){
+    console.log(x+y);
+
+}
+addNum(10,2);*/
+
+
+/*------------------------*/
+
+/*function addNum(x,y){
+    return x+y;
+    
+
+}
+let result=addNum(10,2);
+console.log(result);*/
+
+/*------------------------*/
+
+/*const a = function(){
+    console.log();
+}
+
+const a = () =>{
+
+}*/
