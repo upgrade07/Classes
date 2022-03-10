8867562ba8226911d6792b61af76174cb0a07a6f
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-03-10 23:15:30 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,53 @@
+/*function add(a,b){
+    return a+b;
+}
+
+console.log(add(18,4))*/
+
+
+/*add = (a,b) =>{
+    return a+b;
+}
+console.log(add(18,4))*/
+
+/*mulby2 = n =>n*2
+
+console.log(mulby2(4));*/
+
+/*var x=10 ;
+function a(){
+    console.log(x);
+}
+a()*/
+
+/*var x=40 ;
+function a(){
+    var x=40
+    console.log(x);
+}
+a()*/
+
+
+/*let array=[1,2,3,4]
+
+array.push(10,"Heyy");
+console.log(array);*/
+
+
+let fruits = ['apple' , 'banana','kiwi','orange']
+
+fruits.pop();
+
+console.log(fruits); 
+
+console.log(fruits.pop()); 
+
+console.log(fruits); 
+
+console.log(fruits.push('strawberry'));
+
+console.log(fruits.push('melon'));
+
+console.log(fruits);
+
+
