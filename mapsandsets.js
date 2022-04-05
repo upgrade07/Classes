726ac8794aaf70e2126caf4af724e1bfd9ebf395
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-04-05 19:23:50 +0530

    Add files via upload

---
+++ mapsandsets.js
@@ -0,0 +1,27 @@
+// // let map = new Map()
+ 
+// // map.set(1,"num1")
+// // map.set(2,"num2")
+// // map.set(3,"num3")
+// // map.set(4,"num4")
+
+// // console.log(map)
+
+
+// var s = "sahil"
+// var len = s.length
+
+// function identify(s,len){
+//     var contains = new Map()
+
+//     var str = ""
+//     for(var i =0 ; i < len ; i++){
+//         if(!contains.has(s[i])){
+//             str += s[i]
+//             contains.set(s[i],1)
+//         }
+//     }
+//     console.log(str)
+// }
+
+// identify(s,len)
